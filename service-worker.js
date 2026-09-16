83445d0c0e26242b10b9c0c76a85720ba16d4655
Flamefigther <arondavidse@gmail.com>
2026-09-16 21:24:36 +0200

    Deploy privacy-safe Hevy Analytics app

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="hevy-analytics-public-v3";
-const SHELL=["./","./index.html","./dashboard.html","./manifest.webmanifest","./icon.svg"];
+const CACHE_NAME="hevy-analytics-public-v2";
+const SHELL=["./","./index.html","./dashboard.html","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(SHELL)))});
 self.addEventListener("activate",e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))),self.clients.claim()]))});
 self.addEventListener("fetch",e=>{
